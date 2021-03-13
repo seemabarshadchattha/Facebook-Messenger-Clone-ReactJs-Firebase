ee5f6183b3cabcca058afa7e05de472d27fc8e81
seemabarshad <seemabarshad786@gmail.com>
2021-03-13 22:18:10 +0400

    change message

--- src/App.js
+++ src/App.js
@@ -54,7 +54,7 @@ function App() {
         <FormControl className="app__formControl">
           <Input
             className="app__input"
-            placeholder="Enter messgae ..."
+            placeholder="Enter message ..."
             value={input}
             onChange={(event) => setInput(event.target.value)}
           />
